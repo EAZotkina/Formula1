b428cd45f740f73c9f1b126e498cfde68d624801
“[firstname <zotkinae84@gmail.com>
2021-04-08 18:52:34 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/Tunnel.java
@@ -0,0 +1,30 @@
+package ru.geekbrains;
+
+import java.util.concurrent.Semaphore;
+
+import static ru.geekbrains.MainClass.HALF_CARS_COUNT;
+
+public class Tunnel extends Stage {
+    Semaphore smph = new Semaphore(HALF_CARS_COUNT);
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
